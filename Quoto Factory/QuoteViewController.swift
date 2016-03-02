f267e3730c37fb8c4ba3dedc9652cb5d2e404c1a
javidsheikh <javid.sheikh@me.com>
2016-03-02 12:02:15 +0000

    Refactor code for animation of buttons on quote category VC

--- Quoto Factory/QuoteViewController.swift
+++ Quoto Factory/QuoteViewController.swift
@@ -60,59 +60,42 @@ class QuoteViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        UIView.animateWithDuration(0.2, animations: { () -> Void in
-            self.angerButton.center = CGPointMake(self.angerButton.center.x + 500, self.angerButton.center.y)
-            self.strengthButton.center = CGPointMake(self.strengthButton.center.x - 500, self.strengthButton.center.y)
-            })
-        UIView.animateWithDuration(0.4, animations: { () -> Void in
-            self.sportsButton.center = CGPointMake(self.sportsButton.center.x + 500, self.sportsButton.center.y)
-            self.managementButton.center = CGPointMake(self.managementButton.center.x - 500, self.managementButton.center.y)
-        })
-        UIView.animateWithDuration(0.6, animations: { () -> Void in
-            self.lifeButton.center = CGPointMake(self.lifeButton.center.x + 500, self.lifeButton.center.y)
-            self.loveButton.center = CGPointMake(self.loveButton.center.x - 500, self.loveButton.center.y)
-        })
-        UIView.animateWithDuration(0.8, animations: { () -> Void in
-            self.funnyButton.center = CGPointMake(self.funnyButton.center.x + 500, self.angerButton.center.y)
-            self.inspirationalButton.center = CGPointMake(self.inspirationalButton.center.x - 500, self.strengthButton.center.y)
-        })
-        UIView.animateWithDuration(1, animations: { () -> Void in
-            self.randomButton.center = CGPointMake(self.randomButton.center.x + 500, self.randomButton.center.y)
-            self.artButton.center = CGPointMake(self.artButton.center.x - 500, self.artButton.center.y)
-        })
-        UIView.animateWithDuration(1.2, animations: { () -> Void in
-            self.wisdomButton.center = CGPointMake(self.wisdomButton.center.x + 500, self.wisdomButton.center.y)
-            self.memoriesButton.center = CGPointMake(self.memoriesButton.center.x - 500, self.memoriesButton.center.y)
-        })
-        UIView.animateWithDuration(1.4, animations: { () -> Void in
-            self.motivationalButton.center = CGPointMake(self.motivationalButton.center.x + 500, self.motivationalButton.center.y)
-            self.adversityButton.center = CGPointMake(self.adversityButton.center.x - 500, self.adversityButton.center.y)
-        })
-        UIView.animateWithDuration(1.6, animations: { () -> Void in
-            self.literatureButton.center = CGPointMake(self.literatureButton.center.x + 500, self.literatureButton.center.y)
-            self.philosophyButton.center = CGPointMake(self.philosophyButton.center.x - 500, self.philosophyButton.center.y)
-        })
-        UIView.animateWithDuration(1.8, animations: { () -> Void in
-            self.ambitionButton.center = CGPointMake(self.ambitionButton.center.x + 500, self.ambitionButton.center.y)
-            self.careerButton.center = CGPointMake(self.careerButton.center.x - 500, self.careerButton.center.y)
-        })
-        UIView.animateWithDuration(2, animations: { () -> Void in
-            self.familyButton.center = CGPointMake(self.familyButton.center.x + 500, self.familyButton.center.y)
-            self.friendsButton.center = CGPointMake(self.friendsButton.center.x - 500, self.friendsButton.center.y)
-        })
-        UIView.animateWithDuration(2.2, animations: { () -> Void in
-            self.luckButton.center = CGPointMake(self.luckButton.center.x + 500, self.luckButton.center.y)
-            self.successButton.center = CGPointMake(self.successButton.center.x - 500, self.successButton.center.y)
-        })
-        UIView.animateWithDuration(2.4, animations: { () -> Void in
-            self.challengeButton.center = CGPointMake(self.challengeButton.center.x + 500, self.challengeButton.center.y)
-            self.patienceButton.center = CGPointMake(self.patienceButton.center.x - 500, self.patienceButton.center.y)
-        })
+        self.animateButton(0.2, button: angerButton, direction: 500)
+        self.animateButton(0.2, button: strengthButton, direction: -500)
+        self.animateButton(0.4, button: sportsButton, direction: 500)
+        self.animateButton(0.4, button: managementButton, direction: -500)
+        self.animateButton(0.6, button: lifeButton, direction: 500)
+        self.animateButton(0.6, button: loveButton, direction: -500)
+        self.animateButton(0.8, button: funnyButton, direction: 500)
+        self.animateButton(0.8, button: inspirationalButton, direction: -500)
+        self.animateButton(1.0, button: randomButton, direction: 500)
+        self.animateButton(1.0, button: artButton, direction: -500)
+        self.animateButton(1.2, button: wisdomButton, direction: 500)
+        self.animateButton(1.2, button: memoriesButton, direction: -500)
+        self.animateButton(1.4, button: motivationalButton, direction: 500)
+        self.animateButton(1.4, button: adversityButton, direction: -500)
+        self.animateButton(1.6, button: literatureButton, direction: 500)
+        self.animateButton(1.6, button: philosophyButton, direction: -500)
+        self.animateButton(1.8, button: ambitionButton, direction: 500)
+        self.animateButton(1.8, button: careerButton, direction: -500)
+        self.animateButton(2.0, button: familyButton, direction: 500)
+        self.animateButton(2.0, button: friendsButton, direction: -500)
+        self.animateButton(2.2, button: luckButton, direction: 500)
+        self.animateButton(2.2, button: successButton, direction: -500)
+        self.animateButton(2.4, button: challengeButton, direction: 500)
+        self.animateButton(2.4, button: patienceButton, direction: -500)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func animateButton(duration: Double, button: UIButton, direction: CGFloat) {
+        UIView.animateWithDuration(duration, animations: { () -> Void in
+            button.center = CGPointMake(button.center.x + direction, button.center.y)
+            
+        })
     }
     
     @IBAction func getQuote(sender: UIButton) {
@@ -122,7 +105,6 @@ class QuoteViewController: UIViewController {
 
 
     // MARK: - Navigation
-
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "segueToQuoteDetailVC" {
         let controller = segue.destinationViewController as! QuoteDetailViewController
